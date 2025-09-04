1cf1cfe3d5748e4ad8ce0c49fe7e4d15fd8c689a
tasty-gumi <1021989072@qq.com>
2025-09-04 15:07:13 +0800

    落后指针法删除倒数第n个链表节点
    Signed-off-by: tasty-gumi <1021989072@qq.com>

---
+++ cpp/19.删除链表的倒数第-n-个结点.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=cpp
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ */
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *removeNthFromEnd(ListNode *head, int n) {
+    ListNode dummy(0, head), *fast = &dummy, *slow = &dummy;
+    while (fast) {
+      if (n < 0) {
+        slow = slow->next;
+      }
+      fast = fast->next;
+      n--;
+    }
+    slow->next = slow->next->next;
+    return dummy.next;
+  }
+};
+// @lc code=end
